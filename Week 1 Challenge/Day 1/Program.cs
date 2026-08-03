03c2cb137ec808b2c7ab9cb21dace3c1b8d7aeff
Waskitha Ghaziadiyata <waskithag@gmail.com>
2026-08-03 14:14:06 +0700

    Day 1 Challenge

---
+++ Week 1 Challenge/Day 1/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+void Generate(int x)
+{
+    if (x % 3 == 0)
+    {
+        if (x % 5 == 0)
+        {
+          Console.Write("foobar ");  
+        }
+        else
+        {
+            Console.Write("foo ");
+        }
+    }
+    else if (x % 5 == 0)
+    {
+        Console.Write("bar ");
+    }
+    else
+    {
+        Console.Write($"{x} ");
+    }
+};
+
+int angka = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= angka; i++)
+{
+    Generate(i);
+}
