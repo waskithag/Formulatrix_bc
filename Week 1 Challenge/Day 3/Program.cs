b3d5a36f995aa304ab731eb0350fefd17d2f6d8f
waskithag <waskithag@gmail.com>
2026-08-05 06:30:10 +0700

    adding Stack challenge

---
+++ Week 1 Challenge/Day 3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+Stack<string> stack = new();
+
+void Type(string word)
+{
+    stack.Push(word);
+    Console.WriteLine($"Typed {word}");
+}
+
+void Undo()
+{
+    if (stack.Count > 0)
+    {
+        string word = stack.Pop();
+        Console.WriteLine($"Undid {word}");
+    }
+    else
+    {
+        Console.WriteLine("Stack is empty");
+    }
+}
+
+Type("foo");
+Type("bar");
+Undo();
+Undo();
+Undo();
