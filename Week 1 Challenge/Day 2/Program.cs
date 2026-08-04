ad87dd15cd0ce9fb802024cf3ebe05908305fbac
Waskitha Ghaziadiyata <waskithag@gmail.com>
2026-08-04 11:41:18 +0700

    Update queue challenge

--- Week 1 Challenge/Day 2/Program.cs
+++ Week 1 Challenge/Day 2/Program.cs
@@ -1,7 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 
-class Program
+Queue<string> queue = new();
+
+void Enqueue(string val)
 {
-    
+    queue.Enqueue(val);
+    Console.WriteLine($"Queued {val}");
 }
+
+void Process()
+{
+    if (queue.Count > 0)
+    {
+        string val = queue.Dequeue();
+        Console.WriteLine($"Processed {val}");
+    }
+    else
+    {
+        Console.WriteLine("Queue is empty");
+    }
+}
+
+Enqueue("A"); 
+Enqueue("B"); 
+Process(); 
+Process();
+Process();
