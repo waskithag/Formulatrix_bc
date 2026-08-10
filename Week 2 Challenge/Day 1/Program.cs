82ec667ae02504c2eb3dd83b7f4bba578b13aaa2
Waskitha Ghaziadiyata <waskithag@gmail.com>
2026-08-10 08:25:10 +0700

    Week 2 Day 1 Exercise

---
+++ Week 2 Challenge/Day 1/Program.cs
@@ -0,0 +1,56 @@
+﻿// See https://aka.ms/new-console-template for more information
+void Generate(int x)
+{
+    if (x % 7 == 0)
+    {
+        if (x % 3 == 0)
+        {
+            if (x % 5 == 0)
+            {
+                Console.Write("foobarjazz ");
+            }
+            else
+            {
+                Console.Write("foojazz ");
+            }
+        }
+        else if (x % 5 == 0)
+        {
+            Console.Write("barjazz ");
+        }
+        else
+        {
+            Console.Write("jazz ");
+        }
+    }
+    else if (x % 3 == 0)
+    {
+        if (x % 5 == 0)
+        {
+            Console.Write("foobar ");
+        }
+        else
+        {
+            Console.Write("foo ");
+        }
+    }
+    else if (x % 5 == 0)
+    {
+        Console.Write("bar ");
+    }
+    else
+    {
+        Console.Write($"{x} ");
+    }
+}
+
+// Generate(21);
+// Console.WriteLine();
+// Generate(35);
+// Console.WriteLine();
+// Generate(105);
+int angka = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= angka; i++)
+{
+    Generate(i);
+}
