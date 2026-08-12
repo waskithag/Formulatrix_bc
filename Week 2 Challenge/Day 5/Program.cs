b16f2ced81b478c2738210dc7b18dccc3e84ba82
Waskitha Ghaziadiyata <waskithag@gmail.com>
2026-08-12 13:19:33 +0700

    Week 2 Day 5 Exercise

---
+++ Week 2 Challenge/Day 5/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using CircQueue;
+
+CircularQueue<int> circQueue = new(3);
+
+circQueue.Log(1); 
+circQueue.Log(2); 
+circQueue.Log(3); 
+circQueue.Log(4); 
+circQueue.Log(5); 
+circQueue.Read();
+circQueue.Read();
+circQueue.Read();
+circQueue.Read();
