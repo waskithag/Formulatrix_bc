7ea263fc8cfd5b0b66c37ddc7d19a2a2d99fa1ca
Waskitha Ghaziadiyata <waskithag@gmail.com>
2026-08-12 07:56:30 +0700

    Format and Parsing

---
+++ Playground/Format and Parsing/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+bool result = TimeSpan.TryParse("invalid", out TimeSpan outTimespan);
+Console.WriteLine($"bool value: {result} | value : {outTimespan}");
+
+bool result1 = DateTime.TryParse("invalid", out DateTime outDate);
+Console.WriteLine($"bool value: {result1} | value : {outDate}");
+
+bool result2 = Guid.TryParse("invalid", out Guid outGuid);
+Console.WriteLine($"bool value: {result2} | value : {outGuid}");
+
