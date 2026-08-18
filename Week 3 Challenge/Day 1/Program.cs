992e960262ee9dc67d9ac0261a8246ad5f6c05c4
Waskitha Ghaziadiyata <waskithag@gmail.com>
2026-08-18 11:16:06 +0700

    Week 3 Day 1 Exercise

---
+++ Week 3 Challenge/Day 1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+
+static string Generate(int x)
+{
+    StringBuilder output = new("");
+
+    if (x % 3 == 0)
+    {
+        output.Append("foo");
+    }
+
+    if (x % 4 == 0)
+    {
+        output.Append("baz");
+    }
+
+    if (x % 5 == 0)
+    {
+        output.Append("bar");
+    }
+
+    if (x % 7 == 0)
+    {
+        output.Append("jazz");
+    }
+
+    if (x % 9 == 0)
+    {
+        output.Append("huzz");
+    }
+
+    if (output.ToString() == "")
+    {
+        return x.ToString();
+    }
+    
+    return output.ToString();
+}
+
+int angka = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= angka; i++)
+{
+    Console.Write($"{Generate(i)} ");
+}
